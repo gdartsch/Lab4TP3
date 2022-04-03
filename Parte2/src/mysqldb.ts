9283bf2cc2730572d5cf3c711776e9b559c4e1d4
gdartsch <chocolateconceptual@gmail.com>
2022-04-03 16:06:48 -0300

    Modificados datos para acceso a DB

--- Parte2/src/mysqldb.ts
+++ Parte2/src/mysqldb.ts
@@ -3,8 +3,8 @@ import {createPool} from 'mysql'
 export const cxMysql = createPool({
     host: 'localhost',
     user: 'root',
-    password: '123456',
-    database: 'utn',
+    password: '',
+    database: 'tp4',
     connectionLimit: 100 //100 es el valor por defecto
   });
   
